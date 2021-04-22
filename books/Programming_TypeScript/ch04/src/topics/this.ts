7ab70bac4a66ca5c267e57092917e25e788d3aa1
hhimanshu <harit.himanshu@yahoo.in>
2021-04-22 05:53:55 -0700

    add how to fix type of this keyword using TypeScript

--- books/Programming_TypeScript/ch04/src/topics/this.ts
+++ books/Programming_TypeScript/ch04/src/topics/this.ts
@@ -1,3 +1,23 @@
-function logTransaction() {
-    
+interface Tr {
+    where: string;
+    amount: number;
+    // log: (this: Tr) => void;
+    log: () => void;
 }
+const transaction: Tr = {
+    where: "Drip Coffee",
+    amount: 12.23,
+    log() {
+        console.log(`You spent ${this.amount} at ${this.where}`)
+    }
+}
+
+transaction.log()
+
+const weather = {
+    location: "San Francisco",
+    current: "Sunny",
+    log: transaction.log
+}
+
+weather.log()
